dbc3c0a01a290d4831c4d17738f920bdee1d781f
TanmayBimal <bimaltanmay11@gmail.com>
2025-07-04 14:19:55 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-  plugins: [react(), tailwindcss()],
+  base: "/URL-Shortner",
+  plugins: [react(), tailwindcss()], 
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
