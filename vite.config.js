de6dd962fabb01f26f3a368a95b8f25db060eb56
TanmayBimal <bimaltanmay11@gmail.com>
2025-07-04 13:52:29 +0530

    Fix vite.config.js base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { defineConfig } from "vite"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/URL-Shortner/", // 👈 ADD THIS LINE
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
